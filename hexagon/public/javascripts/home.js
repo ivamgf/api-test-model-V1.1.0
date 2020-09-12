4ee67ad07e279f896f21c6ed9e2f7c9b5c94245c
Ivam <ivam.developer@gmail.com>
2020-09-12 11:36:39 -0300

    Integration Circleci

---
+++ hexagon/public/javascripts/home.js
@@ -0,0 +1,26 @@
+// Home file
+
+// Imports
+
+// Imports Modules
+
+// Imports components
+
+// Variables
+
+// Constant
+const home = `<div class="container">
+                <i class="<i class="fas fa-code"></i> fa-7x"></i>
+                <h2>Grad Core js</h2>
+                <h3>API Hexagon</h3>
+                <p>Version 1.1.0</p>
+            </div>`;
+
+const homepage = { 
+    home: home
+};
+
+// Functions
+
+// Exports
+module.exports.homepage = homepage;
